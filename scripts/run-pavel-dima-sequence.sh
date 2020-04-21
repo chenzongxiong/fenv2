fa6f1e87cb01db13a43f2935e138f23fa382c23b
zongxiongchen <czxczf@gmail.com>
2020-04-21 05:38:11 +0200

    add scripts of paval/dima

--- scripts/run-pavel-dima-sequence.sh
+++ scripts/run-pavel-dima-sequence.sh
@@ -11,17 +11,17 @@
 #SBATCH --mail-user=czxczf@gmail.com
 
 # # elu pavel sequence
-python run_hnn_mse.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 10 --__units__ 10 --__activation__ elu --force_train
-python run_hnn_mse.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 25 --__units__ 10 --__activation__ elu --force_train
-python run_hnn_mse.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 25 --__units__ 25 --__activation__ elu --force_train
-python run_hnn_mse.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 50 --__units__ 25 --__activation__ elu --force_train
-python run_hnn_mse.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 50 --__units__ 50 --__activation__ elu --force_train
-python run_hnn_mse.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 100 --__units__ 50 --__activation__ elu --force_train
+python run_hnn_dima_pavel_seq.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 10 --__units__ 10 --__activation__ elu --force_train
+python run_hnn_dima_pavel_seq.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 25 --__units__ 10 --__activation__ elu --force_train
+python run_hnn_dima_pavel_seq.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 25 --__units__ 25 --__activation__ elu --force_train
+python run_hnn_dima_pavel_seq.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 50 --__units__ 25 --__activation__ elu --force_train
+python run_hnn_dima_pavel_seq.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 50 --__units__ 50 --__activation__ elu --force_train
+python run_hnn_dima_pavel_seq.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 100 --__units__ 50 --__activation__ elu --force_train
 
 # # elu dima sequence
-python run_hnn_mse.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 10 --__units__ 10 --__activation__ elu --force_train
-python run_hnn_mse.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 25 --__units__ 10 --__activation__ elu --force_train
-python run_hnn_mse.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 25 --__units__ 25 --__activation__ elu --force_train
-python run_hnn_mse.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 50 --__units__ 25 --__activation__ elu --force_train
-python run_hnn_mse.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 50 --__units__ 50 --__activation__ elu --force_train
-python run_hnn_mse.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 100 --__units__ 50 --__activation__ elu --force_train
+python run_hnn_dima_pavel_seq.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 10 --__units__ 10 --__activation__ elu --force_train
+python run_hnn_dima_pavel_seq.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 25 --__units__ 10 --__activation__ elu --force_train
+python run_hnn_dima_pavel_seq.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 25 --__units__ 25 --__activation__ elu --force_train
+python run_hnn_dima_pavel_seq.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 50 --__units__ 25 --__activation__ elu --force_train
+python run_hnn_dima_pavel_seq.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 50 --__units__ 50 --__activation__ elu --force_train
+python run_hnn_dima_pavel_seq.py --epochs 1000 --activation tanh --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 100 --__units__ 50 --__activation__ elu --force_train
