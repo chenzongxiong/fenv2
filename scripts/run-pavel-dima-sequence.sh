ff2327dee057af1b0d3db81f874952851a663abf
zxchen <czxczf@gmail.com>
2020-04-22 16:13:48 +0200

    submit multiple jobs

--- scripts/run-pavel-dima-sequence.sh
+++ scripts/run-pavel-dima-sequence.sh
@@ -2,30 +2,65 @@
 
 #SBATCH -J run-paval-dima-sequence
 #SBATCH -D /home/zxchen/fenv2
-#SBATCH -o ./tmp/run_pavel-dima-sequence.log
+#SBATCH -o ./tmp/run-pavel-dima-sequence-%a-%j.log
 #SBATCH --nodes=1
 #SBATCH --mem=80G
-#SBATCH --time=30-00:00:00
-#SBATCH --partition=big
+#SBATCH --time=2-00:00:00
+#SBATCH --partition=big,small
 #SBATCH --mail-type=end
 #SBATCH --mail-user=czxczf@gmail.com
+#SBATCH --array=0-5
+
+__nb_plays__array=(10 25 25 50 50 100)
+__units__array=(10 10 25 25 50 50)
 
 
-hostname
-source /home/zxchen/.venv3/bin/activate
+# input=("bill" "ted" "lisa" "42")
 
-# # elu pavel sequence
-python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 10 --__units__ 10 --__activation__ elu --force_train --diff-weights
-python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 25 --__units__ 10 --__activation__ elu --force_train --diff-weights
-python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 25 --__units__ 25 --__activation__ elu --force_train --diff-weights
-python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 50 --__units__ 25 --__activation__ elu --force_train --diff-weights
-python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 50 --__units__ 50 --__activation__ elu --force_train --diff-weights
-python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 100 --__units__ 50 --__activation__ elu --force_train --diff-weights
+# function echoMe {
+#     echo "Hello $1 from job $SLURM_JOB_ID, $SLURM_ARRAY_TASK_ID"
+#     sleep 1
+#     exit 0
+# }
 
-# # elu dima sequence
-python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 10 --__units__ 10 --__activation__ elu --force_train --diff-weights
-python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 25 --__units__ 10 --__activation__ elu --force_train --diff-weights
-python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 25 --__units__ 25 --__activation__ elu --force_train --diff-weights
-python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 50 --__units__ 25 --__activation__ elu --force_train --diff-weights
-python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 50 --__units__ 50 --__activation__ elu --force_train --diff-weights
-python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 100 --__units__ 50 --__activation__ elu --force_train --diff-weights
+# echoMe ${input[SLURM_ARRAY_TASK_ID]}
+
+function run_pavel {
+    __nb_plays__=$1
+    __units__=$2
+    echo "Run pavel with __nb_plays__=${__nb_plays__}, __units__=${__units__}"
+    # python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ ${__nb_plays__} --__units__ ${__units__} --__activation__ elu --force_train --diff-weights
+    exit 0
+}
+
+function run_dima {
+    __nb_plays__=$1
+    __units__=$2
+    host_name=`hostname`
+    echo "Run dima with __nb_plays__=${__nb_plays__}, __units__=${__units__}, job id ${SLURM_JOB_ID}, task id ${SLURM_ARRAY_TASK_ID}, hostname ${host_name}"
+    source /home/zxchen/.venv3/bin/activate
+    python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ ${__nb_plays__} --__units__ ${__units__} --__activation__ elu --force_train --diff-weights
+    # sleep 30
+    exit 0
+}
+
+run_dima ${__nb_plays__array[SLURM_ARRAY_TASK_ID]} ${__units__array[SLURM_ARRAY_TASK_ID]}
+
+
+# hostname
+
+# # # elu pavel sequence
+# python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 10 --__units__ 10 --__activation__ elu --force_train --diff-weights
+# python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 25 --__units__ 10 --__activation__ elu --force_train --diff-weights
+# python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 25 --__units__ 25 --__activation__ elu --force_train --diff-weights
+# python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 50 --__units__ 25 --__activation__ elu --force_train --diff-weights
+# python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 50 --__units__ 50 --__activation__ elu --force_train --diff-weights
+# python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-pavel --__nb_plays__ 100 --__units__ 50 --__activation__ elu --force_train --diff-weights
+
+# # # elu dima sequence
+# python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 10 --__units__ 10 --__activation__ elu --force_train --diff-weights
+# python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 25 --__units__ 10 --__activation__ elu --force_train --diff-weights
+# python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 25 --__units__ 25 --__activation__ elu --force_train --diff-weights
+# python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 50 --__units__ 25 --__activation__ elu --force_train --diff-weights
+# python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 50 --__units__ 50 --__activation__ elu --force_train --diff-weights
+# python run_hnn_dima_pavel_seq.py --epochs 1000  --mu 0 --sigma 0 --lr 0.05 --points 1000 --nb_plays 50 --units 50 --method debug-dima --__nb_plays__ 100 --__units__ 50 --__activation__ elu --force_train --diff-weights
